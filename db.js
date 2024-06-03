c8dcddb38ea81358eb0743beee66bac7c971a056
전지성 <jisung@jeonjiseong-ui-noteubug.local>
2024-06-04 01:36:27 +0900

    config 파일 구현

--- db.js
+++ db.js
@@ -1,15 +1,29 @@
 const mysql = require('mysql');
+const config = require('./config/config.json');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root', // 사용자 이름
-    password: '1234', // 비밀번호
-    database: 'profilerDB' // 데이터베이스 이름
+    host: config.development.host,
+    user: config.development.username,
+    password: config.development.password
 });
 
-db.connect(err => {
-    if (err) throw err;
-    console.log('Connected to the database!');
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the MySQL database.');
+
+    // 데이터베이스 생성
+    db.query('CREATE DATABASE IF NOT EXISTS profilerDB', (err) => {
+        if (err) throw err;
+        console.log('Database created or already exists.');
+
+        // 데이터베이스 선택
+        db.query('USE profilerDB', (err) => {
+            if (err) throw err;
+        });
+    });
 });
 
 module.exports = db;
